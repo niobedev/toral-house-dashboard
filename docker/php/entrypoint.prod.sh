6005d437200a845dc38716b2820271c47185322c
Victoria Shayner <victoria.shayner@gmail.com>
2026-04-09 11:13:54 +0300

    Enable database migrations in production entrypoint

--- docker/php/entrypoint.prod.sh
+++ docker/php/entrypoint.prod.sh
@@ -4,7 +4,7 @@ set -e
 echo "Warming up cache..."
 php bin/console cache:warmup --env=prod --no-debug
 
-#echo "Running migrations..."
-#php bin/console doctrine:migrations:migrate --no-interaction --env=prod
+echo "Running migrations..."
+php bin/console doctrine:migrations:migrate --no-interaction --env=prod
 
 exec "$@"
